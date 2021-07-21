f091a381ac1455487d852988dac4a166e6e29d5c
Jennifer Kim <jenkim@redhat.com>
2021-07-21 15:59:53 -0400

    added regression graph

--- buildtime_src.py
+++ buildtime_src.py
@@ -1,7 +1,47 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
-df = pd.read_excel(r'YOUR_FILE_PATH/buildtime_src (v14-16 data).xlsx')
-# print(df)
+# import data from buildtime_src (v14-16 data)
+df = pd.read_excel(r'/Users/jenkim/Desktop/Env/build_time_analysis/buildtime_src (v14-16 data).xlsx')
+# data = pd.DataFrame(df, columns=["size", "duration"])
+# print(data)
 
-sample = df.sample(n=300)
-print(sample)
+
+# Select necessary data points
+X = df['size'].to_numpy()
+y = df['duration'].to_numpy()
+# print(X)
+# print(y)
+
+
+# Select random 300 rows with colums size and duration]
+# sample = df.sample(n=300)
+# print(sample)
+
+# Scatter Plot
+
+# df.plot(x='size', y='duration', kind='scatter')
+# plt.show()
+
+
+# Linear Regression
+lin_reg = LinearRegression()
+lin_reg.fit(X.reshape(-1, 1), y)
+
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X.reshape(-1, 1)), color='blue')
+
+# Regression
+mymodel = np.poly1d(np.polyfit(X, y, 2))
+myline = np.linspace(min(X), max(X), len(X))
+plt.scatter(X, y)
+plt.plot(myline, mymodel(myline))
+
+
+# Graph
+plt.title("Build Time Analysis (Size vs. Duration)")
+plt.xlabel('Size')
+plt.ylabel('Duration')
+plt.show()
